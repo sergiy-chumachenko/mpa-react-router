c7e305028b4b4d353a04c44514b80bc747060868
Serhii Chumachenko <chumachenko.sergiy@gmail.com>
2023-02-23 14:12:05 +0200

    update ui state based on submission status

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,9 +1,12 @@
-import {Form, useNavigate} from 'react-router-dom';
+import {Form, useNavigate, useNavigation} from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
   function cancelHandler() {
     navigate('..');
   }
@@ -51,10 +54,10 @@ function EventForm({ method, event }) {
         />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
-          Cancel
+        <button type="button" disabled={isSubmitting} onClick={cancelHandler}>
+          {isSubmitting ? 'Submitting...' : "Cancel"}
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>{isSubmitting ? 'Submitting...' : "Save"}</button>
       </div>
     </Form>
   );
